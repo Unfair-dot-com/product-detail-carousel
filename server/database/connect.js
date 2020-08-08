452aafe09f2602a6c9ec908d8af439cfa4cc31e0
Elvin Kosova <elvinkosova@gmail.com>
2020-08-08 15:04:28 -0700

    Include options argument for Client invocation

--- server/database/connect.js
+++ server/database/connect.js
@@ -2,7 +2,7 @@ const Client = require('mongodb').MongoClient;
 const config = require('../config/db');
 
 const db = (url, dbName) => new Promise((resolve, reject) => {
-  const connection = Client(url);
+  const connection = Client(url, { useUnifiedTopology: true });
   connection.connect((error, client) => {
     if (error) {
       reject(error);
