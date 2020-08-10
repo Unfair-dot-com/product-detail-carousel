eaf382744243e1036e832d2084f66135bd9c21ca
Elvin Kosova <elvinkosova@gmail.com>
2020-08-10 14:55:21 -0700

    Finalize the seeding script

---
+++ server/config/init.js
@@ -0,0 +1,7 @@
+module.exports = {
+  startId: 1,
+  endId: 100,
+  localDir: 'images',
+  remoteDir: 'medium',
+  remotePath: 'https://product-detail-carousel-images.s3-us-west-1.amazonaws.com/',
+};
