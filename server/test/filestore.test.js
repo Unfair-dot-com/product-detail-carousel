89be6603623625698c85ec054f18f3874b5838f5
Elvin Kosova <elvinkosova@gmail.com>
2020-08-11 17:05:02 -0700

    Untested implementation of the basic filestore tests

--- server/test/filestore.test.js
+++ server/test/filestore.test.js
@@ -1,1 +1,45 @@
 const fs = require('../filestore');
+
+const remoteDir = 'test';
+const localDir = './images';
+const images = ['test/image_0.jpg', 'test/image_1.jpg'];
+
+beforeEach((done) => {
+  fs.removeFiles(remoteDir)
+    .then(() => {
+      done();
+    })
+    .catch((error) => {
+      console.log('fs.removeFiles error:', error);
+      done();
+    });
+});
+
+test('Should upload files from local images dir to remote test dir.', (done) => {
+  fs.uploadFiles(localDir, remoteDir)
+    .then(() => fs.listRemoteDir(remoteDir))
+    .then((results) => {
+      expect(results).toEqual(images);
+      done();
+    });
+});
+
+test('Should remove files from remote test dir.', (done) => {
+  fs.uploadFiles(localDir, remoteDir)
+    .then(() => fs.removeFiles(remoteDir))
+    .then(() => fs.listRemoteDir(remoteDir))
+    .then((results) => {
+      expect(results).toEqual([]);
+      done();
+    });
+});
+
+
+test('Should list files in remote test dir.', (done) => {
+  fs.uploadFiles(localDir, remoteDir)
+    .then(() => fs.listRemoteDir(remoteDir))
+    .then((results) => {
+      expect(results).toEqual(images);
+      done();
+    });
+});
