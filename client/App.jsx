ab5240c9281b7ad394b465d2523c37a4b744ad2a
Elvin Kosova <elvinkosova@gmail.com>
2020-08-12 16:27:12 -0700

    Initialize the client App code

---
+++ client/App.jsx
@@ -0,0 +1,21 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  getProducts() {
+    
+  }
+
+  render() {
+    return (
+      <h1>Hello App</h1>
+    );
+  }
+}
+
+module.exports = App;
