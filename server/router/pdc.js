afb36291b80b49791b8e2c716971f0e76c02ca47
Elvin Kosova <elvinkosova@gmail.com>
2020-08-12 13:06:25 -0700

    Implement endpoint router tests

--- server/router/pdc.js
+++ server/router/pdc.js
@@ -1,13 +1,17 @@
 const db = require('../database');
 
 const get = (req, res, next) => {
-  console.log('routes.pdc.get req.body:', req.body);
-  console.log('routes.pdc.get req.params:', req.params);
+  // console.log('routes.pdc.get req.body:', req.body);
+  // console.log('routes.pdc.get req.params:', req.params);
   db.find().then((products) => {
+    // console.log('routes.pdc.get db.find products:', products);
     res.json(products);
     res.end();
   }).catch((error) => {
     console.log('routes.pdc.get db.find error:', error);
+    res.status(500);
+    res.json('Server error');
+    res.end();
   });
 };
 
