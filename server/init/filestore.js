c5470516045c442944518ffb42f02a0368412229
Elvin Kosova <elvinkosova@gmail.com>
2020-08-11 20:42:16 -0700

    Implement image uplaod initialization with fixed init structure

--- server/init/filestore.js
+++ server/init/filestore.js
@@ -4,11 +4,20 @@ const config = require('../config/init');
 const uploadImages = () => {
   fs.removeFiles(config.remoteDir)
     .catch((error) => {
-      console.log('fs.removeFiles error:', error);
-      if (error) {
-        console.log('fs.removeFiles error:', error);
+      if (error.code === 'MalformedXML') {
+        console.log('Remote directory is already empty.');
+        console.log('error.code:', error.code);
+        console.log('error.region:', error.region);
+        console.log('error.time:', error.time);
+        console.log('error.requestId:', error.requestId);
+        console.log('error.extendedRequestId:', error.extendedRequestId);
+        console.log('error.cfId:', error.cfId);
+        console.log('error.statusCode:', error.statusCode);
+        console.log('error.retryable:', error.retryable);
+        console.log('error.retryDelay:', error.retryDelay);
+      } else {
+        throw error;
       }
-      return error;
     })
     .then(() => fs.uploadFiles(config.localDir, config.remoteDir))
     .then(() => fs.listRemoteDir(config.remoteDir))
