d7c650ca72745bc9c17a504e767671775c386250
Elvin Kosova <elvinkosova@gmail.com>
2020-08-13 13:12:24 -0700

    Missed one file

--- client/app/components/product-card/product-name.jsx
+++ client/app/components/product-card/product-name.jsx
@@ -1,27 +1,13 @@
 const React = require('react');
 const styled = require('styled-components');
+const Heading = require('../lib/heading.jsx');
 
 const Name = ({ product }) => {
-  const Container = styled.default.div``;
-  const Heading = styled.default.h2`
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    text-overflow: ellipsis;
-    overflow: hidden;
+  const HeadingName = styled.default(Heading)`
     height: 2.4em;
-    color: #221924;
-    font-weight: 400;
-    font-size: 16px;
-    transition: color .15s ease-out;
-    line-height: 1.2;
-    margin: 0;
-    padding: 0;
-    font-family: sofia,arial,sans-serif;`;
+    margin-top: 8px;`;
   return (
-    <Container>
-      <Heading>{product.name}</Heading>
-    </Container>
+    <HeadingName>{product.name}</HeadingName>
   );
 };
 
