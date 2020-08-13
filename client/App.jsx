d9ce38665c147c2e68d9b5c275bbd056d16bb2bf
Elvin Kosova <elvinkosova@gmail.com>
2020-08-12 17:11:12 -0700

    Implement and manually test the pdc api client

--- client/App.jsx
+++ client/App.jsx
@@ -1,19 +1,40 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
+const axios = require('axios');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      products: [],
+    };
+    this.updateProducts = this.updateProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProducts();
   }
 
   getProducts() {
-    
+    const path = '/api/pdc/';
+    const id = window.location.pathname.match(/\d+/)[0];
+    const uri = `${path}${id}`;
+    axios(uri)
+      .then((response) => this.updateProducts(response.data))
+      .catch((error) => {
+        console.log('getProducts error:', error);
+      });
+  }
+
+  updateProducts(products) {
+    // console.log('updateProducts products:', products);
+    this.setState((prevState, props) => ({ products }));
   }
 
   render() {
     return (
-      <h1>Hello App</h1>
+      <ul>
+        {this.state.products.map((product) => (<li key={product._id}>{product.name}</li>))}
+      </ul>
     );
   }
 }
