5de5ec8d6713d3a9e5676c21a299153a5fbc3382
Elvin Kosova <elvinkosova@gmail.com>
2020-08-19 13:29:30 -0700

    Fix styling on lib components

--- client/app/components/lib/heading.jsx
+++ client/app/components/lib/heading.jsx
@@ -1,18 +1,14 @@
 const style = require('styled-components').default;
 
 const Heading = style.h2`
-  display: block;
-  max-width: 100%;
   margin: 0;
   padding: 0;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: normal;
-  color: #221924;
-  line-height: 1.2;
   font-size: 16px;
   font-weight: 400;
-  font-family: sofia,arial,sans-serif;
-  box-sizing: border-box;`;
+  color: #221924;
+  line-height: 1.2;`;
 
 module.exports = Heading;
