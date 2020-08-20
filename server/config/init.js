fc9dfc0e0ad63a87046b455735c6209769885b4c
Elvin Kosova <elvinkosova@gmail.com>
2020-08-19 20:38:52 -0700

    Refactor backend image processing

--- server/config/init.js
+++ server/config/init.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 module.exports = {
+  remotePath: 'https://product-detail-carousel-images.s3-us-west-1.amazonaws.com/',
   startId: 0,
   endId: 99,
-  localDir: 'images',
-  remoteDir: 'medium',
-  remotePath: 'https://product-detail-carousel-images.s3-us-west-1.amazonaws.com/',
+  localSmallDir: path.resolve('images', 'small'),
+  localMediumDir: path.resolve('images', 'medium'),
+  localLargeDir: path.resolve('images', 'large'),
+  remoteSmallDir: 'small',
+  remoteMediumDir: 'medium',
+  remoteLargeDir: 'large',
 };
